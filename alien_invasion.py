d4806a11b99770369a9662382a914b944760c484
Demir Altay <demiraltay@my.unt.edu>
2022-05-20 00:52:53 -0500

    Updated movement
    Ship no longer goes off-screen. Also refactored code responsible for keypresses into functions.

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,19 +35,30 @@ class AlienInvasion:
                 sys.exit()
 
             # When the key is pressed.
-            elif event.type == pygame.KEYDOWN: 
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event) 
 
             # When the key is released.
             elif event.type == pygame.KEYUP:
+                self._check_keyup_events
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
 
+    def _check_keydown_events(self, event):
+        """Respond to key presses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
         
     def _update_screen(self):
         """Update images on the screen. and flip to the new screen."""
