f99a48276eefcfae645b6306365daf630002d1c1
Demir Altay <demiraltay@my.unt.edu>
2022-05-19 23:07:36 -0500

    Created Ship class
    Now displays the ship on screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,9 @@
 import sys
 import pygame
+from sympy import Shi
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -14,6 +16,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
 
         # Set the background color.
         self.screen.fill(self.settings.bg_color)
@@ -28,7 +32,8 @@ class AlienInvasion:
             
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
-            
+            self.ship.blitme()
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
